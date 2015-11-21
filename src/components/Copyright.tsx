09ee1a6768734c33bd5f2e3be7a231910c90e55a
Tsubasa Namiki <namikingsoft@gmail.com>
2015-11-21 19:27:20 +0900

    Update copyright

--- src/components/Copyright.tsx
+++ src/components/Copyright.tsx
@@ -7,7 +7,13 @@ export default class Navigation extends Component<any, any>
     return (
       <div className="module-copyright">
         <small>
-          Copyright 2015 <i className="fa fa-copyright"></i>namikingsoft
+          <p>Copyright 2015 <i className="fa fa-copyright"></i>namikingsoft</p>
+          <p>Powered By <a href="#">React</a>, <a href="#">Redux</a></p>
+          <ul>
+            <li><i className="fa fa-pie-chart"></i><a href="#">d3.js</a></li>
+            <li><i className="fa fa-align-left"></i><a href="#">NVD3</a></li>
+            <li>and more</li>
+          </ul>
         </small>
       </div>
     )
